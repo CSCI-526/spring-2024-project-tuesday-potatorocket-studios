c7bf3959cde26ab41c695712944df63a36ce6547
Zhila Shariat <zshariat08@gmail.com>
2024-02-21 14:58:14 -0800

    Timer added to top of view with slider

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public Slider slider;
+    public float sliderTimer;
+    private bool timerIsRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = sliderTimer;
+        slider.value = sliderTimer;
+
+        StartTimer();
+    }
+
+
+    public void StartTimer()
+    {
+        timerIsRunning = true;
+        StartCoroutine(UpdateTimer());
+    }
+
+
+    //ticks down the timer
+    IEnumerator UpdateTimer()
+    {
+        while (timerIsRunning)
+        {
+            sliderTimer -= Time.deltaTime;
+            yield return new WaitForSeconds(.001f);
+            if (sliderTimer <= 0)
+            {
+                timerIsRunning = false;
+            }
+
+            slider.value = sliderTimer;
+        }
+    }
+
+
+}
