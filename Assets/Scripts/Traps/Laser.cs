0af6bef2c123f53b3c8a190a29895dd509437f1e
Daksh Goyal <dakshgoyal58@gmail.com>
2024-04-14 13:33:12 -0700

    changed fixeddeltatime to deltatime

--- Assets/Scripts/Traps/Laser.cs
+++ Assets/Scripts/Traps/Laser.cs
@@ -54,7 +54,7 @@ public class Laser : MonoBehaviour
             direction = target.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
             // Perform the raycast and set the line renderer positions
             raycastDirection = transform.right;
